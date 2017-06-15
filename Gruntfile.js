d682bb8e3fc137d3edb60138faf43aa7e9c7c984
Masaki Ihara <masaki0320@me.com>
2017-06-15 17:09:30 +0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    // Configuration to be run (and then tested).
+    html_pdf: {
+      example: {
+        files: {
+          'examples/example.pdf': ['examples/example.html',],
+        }
+      }
+    },
+    bump: {
+      options: {
+        push: false,
+        pushTo: "origin",
+        files: [
+          "package.json",
+        ]
+      }
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-bump');
+
+  grunt.registerTask('default', ['jshint', ]);
+
+};
